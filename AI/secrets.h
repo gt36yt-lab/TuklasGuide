c5740a75ce5a63930e47df3f3d93d0169a18a713
Aliiiiiiiii <123goyen1@gmail.com>
2026-06-27 20:39:11 +0800

    Delete AI directory

--- AI/secrets.h
+++
@@ -1,6 +0,0 @@
-// Replace with your WiFi network credentials
-#define WIFI_SSID "WTF"
-#define WIFI_PASSWORD "00000000"
-
-// Replace with your Google AI Studio API Key
-#define GEMINI_API_KEY "AIzaSyB78Rf8KeI4mln0KjlFTaj23zX3J45lwuM"
